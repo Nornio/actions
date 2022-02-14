5818596b8697d1bc01bac50f198b4b40d79e3099
Stefan Sik <stefan.sik@nornio.net>
2022-02-14 11:03:39 +0100

    Minified dist

--- jira-client/dist/JiraIssueParser.d.ts
+++
@@ -1,7 +0,0 @@
-declare class JiraIssueParser {
-    private projectKey;
-    constructor(projectKey: string);
-    parseIssuesKeysFromLog(log: string[]): string[];
-    parseLineForIssues(line: string): string[] | null;
-}
-export default JiraIssueParser;
